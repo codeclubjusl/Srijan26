1b5e1e8dc42159cd571f4aa2fc64d8c27b78b195
Prantik Das <prantik.ts@gmail.com>
2026-02-23 19:22:17 +0530

    feat: changed landing page gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
-import Balls from "@/components/Balls";
 import { HeroSection } from "@/components/Landing/HeroSection";
 import { Carousel } from "@/components/Landing/ImageCarousel";
 import { PastSponsors } from "@/components/Landing/PastSponsors";
 import Timeline from "@/components/Landing/Timeline";
 import LiveEvents from "@/components/Landing/LiveEvents";
+import WavyGradient from "@/components/WavyGradient";
 
 export default function Home() {
   return (
     <>
+      {/* Background Gradient */}
+      <WavyGradient
+        color1="#bc6116"
+        color2="#8f0c03"
+        color3="#1A0000"
+        direction={20}
+        speed={1.5}
+        waveHeight={0.45}
+        noiseIntensity={5}
+        waveAmplitude={1}
+      />
       <HeroSection />
-      <Balls />
       <Timeline />
       <LiveEvents />
       <PastSponsors />
